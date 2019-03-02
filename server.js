7de712c419c1052183603b53471782a7d57efcd9
Joao Buch <joaoguibune>
2019-03-02 09:40:45 -0300

    adding heroku procfile

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('/dist/medlog'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/medilog'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('/dist/medlog/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/medilog/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
